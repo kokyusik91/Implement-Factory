00a3e041eff495cbbc0f24dfaa4fe1f01b9a398b
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2022-12-23 00:50:07 +0900

    🏷️ 챔피언 타입 정의

---
+++ Riot-api/types.ts
@@ -0,0 +1,53 @@
+export type Champion = {
+  blurb: string
+  id: string
+  image: ChampionImage
+  info: ChampionInfo
+  key: string
+  name: string
+  partype: string
+  stats: ChampionStat
+  tags: string[]
+  title: string
+  version: string
+}
+
+type ChampionImage = {
+  full: string
+  group: string
+  h: number
+  sprite: string
+  w: number
+  x: number
+  y: number
+}
+
+type ChampionInfo = {
+  attack: number
+  defense: number
+  difficulty: number
+  magic: number
+}
+
+type ChampionStat = {
+  armor: number
+  armorperlevel: number
+  attackdamage: number
+  attackdamageperlevel: number
+  attackrange: number
+  attackspeed: number
+  attackspeedperlevel: number
+  crit: number
+  critperlevel: number
+  hp: number
+  hpperlevel: number
+  hpregen: number
+  hpregenperlevel: number
+  movespeed: number
+  mp: number
+  mpperlevel: number
+  mpregen: number
+  mpregenperlevel: number
+  spellblock: number
+  spellblockperlevel: number
+}
