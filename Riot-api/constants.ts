a6f032e87bc2b28d8cf0654d8c01eeb5176ffe32
kks91 <kokyusik91@naver.com>
2022-12-25 10:20:45 +0900

    🚧 마크업 및 스타일

---
+++ Riot-api/constants.ts
@@ -0,0 +1,5 @@
+export const api = {
+  BASE_URL: 'https://kr.api.riotgames.com',
+  ROTATION_CHAMP_URL: '/lol/platform/v3/champion-rotations',
+  RIOT_API_KEY: 'RGAPI-d1f4583f-ecef-4125-beb1-98ba455e3abd',
+}
