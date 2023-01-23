6975165aff6836a5acf63147341c240e66223b55
kks91 <kokyusik91@naver.com>
2023-01-23 15:57:32 +0900

    🚧 update keyboard class and implement dark theme

---
+++ keyboard/src/js/keyboard.js
@@ -0,0 +1,21 @@
+export class Keyboard {
+  #switchEl;
+  constructor() {
+    this.#assignElement();
+    this.#addEvent();
+  }
+
+  #assignElement() {
+    this.#switchEl = document.getElementById("switch");
+  }
+
+  #addEvent() {
+    this.#switchEl.addEventListener("change", (e) => {
+      document.documentElement.setAttribute(
+        "theme",
+        e.target.checked ? "dark-mode" : ""
+      );
+      console.log(e.target.checked);
+    });
+  }
+}
