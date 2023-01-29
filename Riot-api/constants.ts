7c28b6a921e5863460742b9cd58267d6d42f1f87
kks91 <kokyusik91@naver.com>
2023-01-29 23:11:08 +0900

    ⏪

--- Riot-api/constants.ts
+++ Riot-api/constants.ts
@@ -1,5 +1,5 @@
 export const api = {
   BASE_URL: 'https://kr.api.riotgames.com',
   ROTATION_CHAMP_URL: '/lol/platform/v3/champion-rotations',
-  RIOT_API_KEY: 'RGAPI-2dac4654-991e-4355-ae3c-f2b1077640ba',
+  RIOT_API_KEY: 'RGAPI-40c022d2-7df4-4c9a-85b5-a01264a88556',
 }
