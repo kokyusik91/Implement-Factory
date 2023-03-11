6aaa5584e14c359c3d0b8b47249d95d008e53495
kks91 <kokyusik91@naver.com>
2023-03-11 23:56:13 +0900

    🚧 정리

---
+++ test3.js
@@ -0,0 +1,58 @@
+// 1. 변수
+
+// 2. 타입
+
+// number
+// string
+// boolean
+// undefined
+// null
+
+// 문자열 + 문자열
+// const a = '4'
+// const b = 3
+// console.log(a + b)
+
+// ===
+// !==
+
+// 조건문
+
+// if
+// falsy
+
+// while
+
+const 이름 = 'kyusik'
+// 삼항 연산자
+const 답 = 이름 === 'kyusiki' ? '이건 답이다' : '이건 답 아님'
+
+// switch
+
+// 중첩 반복문
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 10; j++) {
+
+//   }
+// }
+
+// 배열
+const 어레이 = [1, 2, 3, 4, 5]
+
+어레이.push('이거추가')
+
+console.log(어레이)
+
+어레이.pop()
+
+console.log(어레이)
+
+// 함수
+
+function 함수(a, b) {
+  ㄱㄷ셔구
+}
+
+함수(1, 2)
+
+const 표현식 = function () {}
